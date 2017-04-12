0730b6d7fecf584fe26e6d1d39202cc3fff25027
RuzzZZ <xzy199400@foxmail.com>
2017-04-12 19:14:43 +0800

    consumer init

--- src/main/java/com/xzy/mq/consumer/AbstractMessageConsumer.java
+++ src/main/java/com/xzy/mq/consumer/AbstractMessageConsumer.java
@@ -1,7 +1,10 @@
 package com.xzy.mq.consumer;
 
+import com.xzy.mq.common.AbstractMessageConfig;
+
 /**
  * Created by RuzzZZ on 2017/4/12.
  */
-public class AbstractMessageConsumer {
+public abstract class AbstractMessageConsumer extends AbstractMessageConfig {
+
 }
